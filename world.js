cf16c35eb22c1de989e512b43453cbaa2b0e7c51
darbdude <darbdude@users.noreply.github.com>
2018-03-28 02:53:59 -0700

    f
    fe

---
+++ world.js
@@ -0,0 +1,23 @@
+room.onLoad = function() {
+
+}
+
+room.update = function(dt) {
+
+}
+
+function LightOff() 
+{    
+  room.objects["OmniLight"].light_intensity = 0;
+}
+
+function LightOn() 
+{    
+  room.objects["OmniLight"].light_intensity = 200; 
+}
+
+function SetLightProperties()
+{
+	room.objects["OmniLight"].col="#2668b2";
+	room.objects["OmniLight"].light_intensity = 16;
+}
