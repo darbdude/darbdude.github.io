b525b3832f5742b56565b7d4ce3ded2e1538fa60
darbdude <darbdude@users.noreply.github.com>
2018-03-29 11:56:34 -0700

    afkroom
    underconstruction

---
+++ js/world.js
@@ -0,0 +1,42 @@
+﻿room.onLoad = function() {
+    StartMusic();
+}
+
+room.update = function(p_dt) {
+    
+
+}
+
+function LightOff() 
+{    
+    room.objects["OmniLight01"].col = "#ff0000";
+    room.objects["OmniLight01"].light_intensity = 16;
+
+
+}
+
+function LightOn() 
+{    
+    room.objects["OmniLight01"].col = "0 1 0";
+    room.objects["OmniLight01"].light_intensity = 160;
+
+}
+
+
+
+
+
+function SetLightProperties()
+{
+    room.objects["OmniLight01"].col="#2668b2";
+    room.objects["OmniLight01"].light_intensity = 16;
+}
+
+function StartMusic()
+    {
+        room.playSound("backgroundsound01");
+}
+
+function StartSphere() {
+    room.playSound("backgroundsound01");
+}
